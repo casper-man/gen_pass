f6ceab79254adee20286641c23423cac6f85a148
PokazannikovMS <casper-man@yandex.ru>
2022-08-22 11:37:17 +0300

    2022-08-22_11-36
    to ini config

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # gen_pass
  Python генератор паролей
 
-## local_settings.py
+## conf.ini
  LENGTH - Длина пароля
  CHARS - Набор символов для создания пароля '+-/*!&$#?=@<>abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
- USERS - Cписок пользователей
+ USERS - Cписок пользователей через запятую (user1,user2,..,userN)
--- main.py
+++ main.py
@@ -1,18 +1,30 @@
 # -*- coding:utf -8 -*-
 #!/usr/bin/python3
 
-from local_settings import USERS, LENGTH, CHARS
-from alive_progress import alive_bar
 import time
 import random
+import configparser
+import os
 
-with alive_bar(len(USERS), dual_line=True, title='Генерация паролей:') as bar:
+
+def main():
+    config = configparser.ConfigParser()
+    config.read('conf.ini')
+
+    LENGTH = int(config['main']['LENGTH'])
+    CHARS = config['main']['CHARS']
+    CHARS_FULL = config['main']['CHARS_FULL'].split()
+    USERS = config['users']['USERS'].split(',')
+    
     with open('passwords.txt', 'w', encoding='utf-8') as file:
         for user in range(len(USERS)):
-            bar.text = f' ->Генерация пароля для пользователя: {USERS[user]}'
             password =''
             for i in range(LENGTH):
                 password += random.choice(CHARS)
-            file.write(f'{USERS[user]:35s}\t{password}\n')
-            time.sleep(0.005)
-            bar()
+            file.write(f'{USERS[user]}\n@modul.tech\n{password}\n\n')
+            print(f'{USERS[user]}\n@modul.tech\n{password}\n\n')
+    os.startfile('passwords.txt')
+
+
+if __name__ == '__main__':
+    main()
